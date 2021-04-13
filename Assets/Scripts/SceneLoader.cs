fecfd5389f684f5f1ce57c25e1e8fce277cc52a8
aleksaradic <aleksaradic01@gmail.com>
2021-04-13 09:14:06 +0200

    Style first Scene

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadWelcomeScene()
+    {
+        SceneManager.LoadScene("00questionOne");
+    }
+
+    public void LoadColorScene()
+    {
+        SceneManager.LoadScene("01questionTwo");
+    }
+
+    public void LoadNumberScene()
+    {
+        SceneManager.LoadScene("02answer");
+    }
+}
