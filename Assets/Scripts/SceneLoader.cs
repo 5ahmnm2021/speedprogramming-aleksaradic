c0587eebfe36e0853b0fa7aae3a49148d466318d
aleksaradic <aleksaradic01@gmail.com>
2021-04-13 09:42:23 +0200

    Finale Abgabe

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -7,18 +7,39 @@ public class SceneLoader : MonoBehaviour
 {
     public int answers;
 
-    public void LoadQuestionOne()
+    public void Restart()
     {
+        StartCoroutine(LoadRestart());
+    }
+
+    public void QuestionOneRight()
+    {
+        StartCoroutine(LoadQuestionOneRight());
+    }
+
+    public void QuestionTwoRight()
+    {
+        StartCoroutine(LoadQuestionTwoRight());
+    }
+
+    IEnumerator LoadRestart()
+    {
+        answers = 0;
+        yield return new WaitForSeconds(2f);
         SceneManager.LoadScene("00questionOne");
     }
 
-    public void LoadQuestionTwo()
+    IEnumerator LoadQuestionOneRight()
     {
+        answers = answers + 1;
+        yield return new WaitForSeconds(2f);
         SceneManager.LoadScene("01questionTwo");
     }
 
-    public void LoadAnswer()
+    IEnumerator LoadQuestionTwoRight()
     {
+        answers = answers + 1;
+        yield return new WaitForSeconds(2f);
         SceneManager.LoadScene("02answer");
     }
 }
